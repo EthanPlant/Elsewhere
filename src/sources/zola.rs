b674104729ac469eb1a4621e32ca9d227a4254e8
Ethan <plant.ethan@gmail.com>
2026-07-28 22:58:22 -0700

    Support both zola.toml and config.toml for Zola sites (#15)

--- src/sources/zola.rs
+++ src/sources/zola.rs
@@ -4,6 +4,7 @@ use std::path::{Path, PathBuf};
 use anyhow::{Context, Result};
 use serde::Deserialize;
 
+use crate::error::ElsewhereError;
 use crate::post::CanonicalPost;
 use crate::sources::Source;
 use crate::sources::markdown;
@@ -24,7 +25,9 @@ impl ZolaSource {
     }
 
     pub fn read_config(&self) -> Result<ZolaSiteConfig> {
-        let path = self.site_root.join("zola.toml");
+        let path = self
+            .find_zola_config()
+            .ok_or(ElsewhereError::SourceConfigNotFound)?;
 
         let raw = fs::read_to_string(&path)
             .with_context(|| format!("failed to read Zola config: {}", path.display()))?;
@@ -34,10 +37,84 @@ impl ZolaSource {
 
         Ok(config)
     }
+
+    fn find_zola_config(&self) -> Option<PathBuf> {
+        let zola_toml = self.site_root.join("zola.toml");
+        if zola_toml.is_file() {
+            return Some(zola_toml);
+        }
+        let config_toml = self.site_root.join("config.toml");
+        if config_toml.is_file() {
+            return Some(config_toml);
+        }
+
+        None
+    }
 }
 
 impl Source for ZolaSource {
     fn read_post(&self, path: &Path) -> Result<CanonicalPost> {
         markdown::read_markdown_post(path)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::{assert_matches, fs};
+
+    use tempfile::tempdir;
+
+    #[test]
+    fn uses_zola_toml_if_present() {
+        let dir = tempdir().unwrap();
+
+        let raw = "base_url = \"https://example.com\"";
+        let file = dir.path().join("zola.toml");
+        fs::write(file, raw).unwrap();
+
+        let zola_source = ZolaSource::new(dir.path().to_path_buf());
+        let zola_config = zola_source.read_config().unwrap();
+        assert_eq!(zola_config.base_url, "https://example.com")
+    }
+
+    #[test]
+    fn fallback_config_toml() {
+        let dir = tempdir().unwrap();
+
+        let raw = "base_url = \"https://example.com\"";
+        let file = dir.path().join("config.toml");
+        fs::write(file, raw).unwrap();
+
+        let zola_source = ZolaSource::new(dir.path().to_path_buf());
+        let zola_config = zola_source.read_config().unwrap();
+        assert_eq!(zola_config.base_url, "https://example.com")
+    }
+
+    #[test]
+    fn prefers_zola_toml() {
+        let dir = tempdir().unwrap();
+
+        let raw = "base_url = \"https://zola.toml\"";
+        let file = dir.path().join("zola.toml");
+        fs::write(file, raw).unwrap();
+
+        let raw = "base_url = \"https://config.toml\"";
+        let file = dir.path().join("config.toml");
+        fs::write(file, raw).unwrap();
+
+        let zola_source = ZolaSource::new(dir.path().to_path_buf());
+        let zola_config = zola_source.read_config().unwrap();
+        assert_eq!(zola_config.base_url, "https://zola.toml")
+    }
+
+    #[test]
+    fn errors_if_no_config() {
+        let dir = tempdir().unwrap();
+
+        let zola_source = ZolaSource::new(dir.path().to_path_buf());
+        let config_result = zola_source.read_config();
+        assert_matches!(config_result, Err(_))
     }
 }
